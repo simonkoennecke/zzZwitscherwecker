6b548932fbe640beaa473b1f75812efee5082357
Simon Könnecke <simonkoennecke@gmail.com>
2014-07-02 23:42:00 +0200

    remove folder of the build process for the app

--- platforms/android/CordovaLib/ant-gen/R.java.d
+++
@@ -1,1 +0,0 @@
- : C:\Users\simon_000\ownCloud\Webseite\app\zzZwitscherwecker\platforms\android\CordovaLib\ant-build\AndroidManifest.xml \
--- platforms/android/ant-build/zzZwitscherwecker-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\Users\simon_000\ownCloud\Webseite\app\zzZwitscherwecker\platforms\android\ant-build\zzZwitscherwecker-debug-unaligned.apk : \
-C:\Users\simon_000\ownCloud\Webseite\app\zzZwitscherwecker\platforms\android\ant-build\zzZwitscherwecker.ap_ \
-C:\Users\simon_000\ownCloud\Webseite\app\zzZwitscherwecker\platforms\android\ant-build\classes.dex \
-C:\Users\simon_000\ownCloud\Webseite\app\zzZwitscherwecker\platforms\android\CordovaLib\ant-build\classes.jar \
